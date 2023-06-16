a0c91302c1b5c20d9d371cad9a49019a3e896a0a
mirek <miroslav.kulha1@homecredit.vn>
2023-06-16 12:07:29 +0700

    feat: download deb file during instalation

--- src/helper/install.sh
+++ src/helper/install.sh
@@ -3,6 +3,8 @@ set -e
 
 echo "Activating feature 'azure devops login helper'"
 
+wget -O "$(dirname $0)/gcm-linux_amd64.2.1.2.deb" https://github.com/git-ecosystem/git-credential-manager/releases/download/v2.1.2/gcm-linux_amd64.2.1.2.deb
 dpkg -i "$(dirname $0)/gcm-linux_amd64.2.1.2.deb"
 git-credential-manager configure
 git config --global credential.credentialStore plaintext
+rm -rf "$(dirname $0)/gcm-linux_amd64.2.1.2.deb"
